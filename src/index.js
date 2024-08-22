4ee6dcaae42ef1a1a0db9b25bef8d2d8ed7ad9d7
Mark Neumann <mark.neumann.sm@gmail.com>
2024-08-22 17:09:06 +0300

    feat: create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+
+  for (let char of str) {
+    let config = bracketsConfig.find(pair => pair.includes(char));
+    if (!config) {
+      continue;
+    }
+
+    let [open, close] = config;
+
+    if (char === open) {
+      if (open === close && stack[stack.length - 1] === open) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (stack.length === 0 || stack.pop() !== open) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
